8d7fe0516aa06953774a2a86c3627bcaa3d45bb8
xiliangxiaoke <shanglihero@163.com>
2015-08-29 19:20:52 +0800

    teeess

--- src/club/xiaoke/supercood/constants/CoordType.java
+++ src/club/xiaoke/supercood/constants/CoordType.java
@@ -16,6 +16,27 @@ public class CoordType {
 	 * baidu
 	 */
 	public static int CT_BD09 = 0X0003;
+	/**
+	 * google earth
+	 */
+	public static int CT_GOOGLE_EARTH = CT_WGS84;
+	/**
+	 * GPS模块
+	 */
+	public static int CT_GPS_DEVICE = CT_WGS84;
 	
+	/**
+	 * 谷歌地图
+	 */
+	public static int CT_GOOGLE_MAP = CT_GCJ02;
+	/**
+	 * 高德地图
+	 */
+	public static int CT_AMAP_GAODE = CT_GCJ02;
+	
+	/**
+	 * 腾讯地图
+	 */
+	public static int CT_TECENT = CT_GCJ02;
 
 }
