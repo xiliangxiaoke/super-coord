5a5ac8d3b96ffa2abce10f10a2e02dd11cd0ba52
xiliangxiaoke <shanglihero@163.com>
2015-08-29 15:28:31 +0800

    first commit project

---
+++ src/club/xiaoke/supercood/MainActivity.java
@@ -0,0 +1,22 @@
+package club.xiaoke.supercood;
+
+//import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+/**
+ * @author SHANG01
+ * 主程序界面
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+   
+}
