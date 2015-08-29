84a7b8a2eb736d1f666007f910c80b2cb5ce2623
xiliangxiaoke <shanglihero@163.com>
2015-08-29 16:37:47 +0800

    aaa

--- src/club/xiaoke/supercood/constants/CoordType.java
+++ src/club/xiaoke/supercood/constants/CoordType.java
@@ -8,10 +8,12 @@ public class CoordType {
 	public static int CT_WGS84 = 0X0001;
 	/**
 	 * 国测局
+	 * guo jia ce hui ju
 	 */
 	public static int CT_GCJ02 = 0X0002;
 	/**
 	 * 百度
+	 * baidu
 	 */
 	public static int CT_BD09 = 0X0003;
 
