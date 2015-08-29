766639f945b15de065d88c8a7e3cceefe72b33f3
xiliangxiaoke <shanglihero@163.com>
2015-08-29 16:32:28 +0800

    提交常量类

---
+++ src/club/xiaoke/supercood/constants/CoordType.java
@@ -0,0 +1,18 @@
+package club.xiaoke.supercood.constants;
+
+public class CoordType {
+	
+	/**
+	 * wgs84
+	 */
+	public static int CT_WGS84 = 0X0001;
+	/**
+	 * 国测局
+	 */
+	public static int CT_GCJ02 = 0X0002;
+	/**
+	 * 百度
+	 */
+	public static int CT_BD09 = 0X0003;
+
+}
