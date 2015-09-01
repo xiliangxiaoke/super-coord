5df5e3d0d92a0e7b02fe595eb9176ca984bd5919
xiliangxiaoke <shanglihero@163.com>
2015-09-01 18:22:39 +0800

    lalalalalalal

--- src/club/xiaoke/supercood/constants/CoordType.java
+++ src/club/xiaoke/supercood/constants/CoordType.java
@@ -5,38 +5,38 @@ public class CoordType {
 	/**
 	 * wgs84
 	 */
-	public static int CT_WGS84 = 0X0001;
+	public static final int CT_WGS84 = 0X0001;
 	/**
 	 * 国测局
 	 * guo jia ce hui ju 
 	 */
-	public static int CT_GCJ02 = 0X0002;
+	public static final  int CT_GCJ02 = 0X0002;
 	/**
 	 * 百度
 	 * baidu
 	 */
-	public static int CT_BD09 = 0X0003;
+	public static final  int CT_BD09 = 0X0003;
 	/**
 	 * google earth
 	 */
-	public static int CT_GOOGLE_EARTH = CT_WGS84;
+	public static final  int CT_GOOGLE_EARTH = 0x0004; //= CT_WGS84;
 	/**
 	 * GPS模块
 	 */
-	public static int CT_GPS_DEVICE = CT_WGS84;
+	public static final  int CT_GPS_DEVICE = 0x0005; //= CT_WGS84;
 	
 	/**
 	 * 谷歌地图
 	 */
-	public static int CT_GOOGLE_MAP = CT_GCJ02;
+	public static final  int CT_GOOGLE_MAP = 0x0006; //= CT_GCJ02;
 	/**
 	 * 高德地图
 	 */
-	public static int CT_AMAP_GAODE = CT_GCJ02;
+	public static final  int CT_AMAP_GAODE = 0x0007; //= CT_GCJ02;
 	
 	/**
 	 * 腾讯地图
 	 */
-	public static int CT_TECENT = CT_GCJ02;
+	public static final  int CT_TECENT = 0x0008; //= CT_GCJ02;
 
 }
